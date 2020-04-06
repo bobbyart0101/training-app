e679fe088bf24f44b4c5bc454993cb283d17cf1d
elena <elvira1598@gmail.com>
2020-04-06 16:09:52 +0200

    add ion refresh

--- src/app/pages/training-set/training-set.page.ts
+++ src/app/pages/training-set/training-set.page.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 import {IonItemSliding, ModalController} from '@ionic/angular';
 import {take} from 'rxjs/operators';
@@ -7,17 +7,19 @@ import {ApiService} from '../../services/api/api.service';
 import {DatePipe} from '@angular/common';
 import {CreatingTrainingSetComponent} from '../../components/creating-training-set/creating-training-set.component';
 import {SetModel} from '../../shared/set.model';
+import {Subscription} from 'rxjs';
 
 @Component({
     selector: 'app-training-set',
     templateUrl: './training-set.page.html',
     styleUrls: ['./training-set.page.scss'],
 })
-export class TrainingSetPage implements OnInit {
+export class TrainingSetPage implements OnInit, OnDestroy {
     private id;
     private typeName;
     trainingSets: any;
     private datePipe = DatePipe;
+    private setSubscription: Subscription;
 
     constructor(private router: Router,
                 private route: ActivatedRoute,
@@ -34,12 +36,30 @@ export class TrainingSetPage implements OnInit {
     }
 
     ionViewWillEnter() {
-        this.trainingSets = this.apiService.getTrainingType(this.id);
+        this.setSubscription = this.apiService.getTrainingType(this.id).subscribe((res) => {
+            this.trainingSets = res;
+        });
+    }
+
+
+    ngOnDestroy() {
+        this.setSubscription.unsubscribe();
+    }
+
+    ionViewDidLeave() {
+    }
+
+    doRefresh(event) {
+        console.log('Begin async operation');
+        this.trainingSets = this.apiService.getTrainingType(this.id).subscribe((res) => {
+            this.trainingSets = res;
+            event.target.complete();
+        });
     }
 
     onAddSet() {
-        console.log( this.id);
-        this.modalCtrl.create({component: CreatingTrainingSetComponent, componentProps: {type: this.id }}).then(modalEl => {
+        console.log(this.id);
+        this.modalCtrl.create({component: CreatingTrainingSetComponent, componentProps: {type: this.id}}).then(modalEl => {
             modalEl.present().then();
             return modalEl.onDidDismiss();
         }).then(resultData => {
@@ -57,7 +77,7 @@ export class TrainingSetPage implements OnInit {
         });
     }
 
-    onDeleted(slidingItem: IonItemSliding,  set: SetModel) {
+    onDeleted(slidingItem: IonItemSliding, set: SetModel) {
         slidingItem.close();
     }
 
