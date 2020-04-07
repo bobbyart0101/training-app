810fe6ebc3e6fed5c0e72e4c10e745c00fd3b201
elena <elvira1598@gmail.com>
2020-04-07 08:21:23 +0200

    complete chart

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -1,24 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ApiService} from '../../services/api/api.service';
+
 @Component({
-  selector: 'app-table',
-  templateUrl: './table.component.html',
-  styleUrls: ['./table.component.scss'],
+    selector: 'app-table',
+    templateUrl: './table.component.html',
+    styleUrls: ['./table.component.scss'],
 })
 export class TableComponent implements OnInit {
-  tableStyle =  'material';
+    tableStyle = 'material';
 
-  rows = [
-    { name: 'Austin', gender: 'Male', company: 'Swimlane' },
-    { name: 'Dany', gender: 'Male', company: 'KFC' },
-    { name: 'Molly', gender: 'Female', company: 'Burger King' },
-  ];
-  columns = [
-    { prop: 'name' },
-    { name: 'Gender' },
-    { name: 'Company' }
-  ];
-  constructor() { }
+    rows = [
+        {weight: 'Austin', rep: 'Male'},
+        {weight: 'Dany', rep: 'Male'},
+        {weight: 'Molly', rep: 'Female'},
+    ];
+    columns = [
+        {name: 'Weight'},
+        {name: 'Rep'}
+    ];
 
-  ngOnInit() {}
+    constructor(private apiService: ApiService) {
+    }
+
+    ngOnInit() {
+        this.apiService.searchResult.subscribe((res) => {
+            const tableResult = res[1];
+            const updateRows = [];
+            console.log(tableResult);
+            tableResult.forEach((row) => {
+              const updateRow = {weight: row[0], rep: row[1]};
+              updateRows.push(updateRow);
+            });
+            this.rows = [...updateRows];
+        });
+    }
 
 }
